ab9f05dd53ab34fe955176b856d908465569e65c
GSaillenfest <104986291+GSaillenfest@users.noreply.github.com>
2022-09-14 16:34:06 +0200

    fin du 8eme jour
    Ajout d'un ennemie, mécanique de chasse et évasion.
    
    Travail sur l'éclairage et l'ambiance globale.

---
+++ UnityFolder/Assets/EnemyScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f161cb52dac3546a3a90c80ae7bc520b95c17aa494335a524fe2021a8b0af31
+size 1822
--- UnityFolder/Assets/PlayerController.cs
+++ UnityFolder/Assets/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24e61c98b98e0a81b067ffad3adc8ef364f719b37ce93c4e7591db88ceb38e08
-size 2371
+oid sha256:2186040ceef21383f8ccbcbbbc51db269b34b436decb2c6bf0933be1fc613d3e
+size 3096
